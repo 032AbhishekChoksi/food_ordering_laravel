02bd10320488ed8e1779db29a904a392d5b8b232
032AbhishekChoksi <19bmiit032@gmail.com>
2022-06-21 20:18:56 +0530

    first commit

---
+++ database/migrations/2022_06_21_141202_create_dishes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('dish_name', 50);
+            $table->text('dish_desc');
+            $table->string('image', 100);
+            $table->enum('type', ["veg", "non-veg"]);
+            $table->tinyInteger('status')->default(1);
+            $table->foreign('category_id')->references('category_id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
